4d81c2c73f8c5af51d2743a818723e0226092bb3
Carole Bou <35@p4-5.hbtn.io>
2016-01-31 20:10:53 -0800

    Push Hello, World project

---
+++ hello_world/1-hello_Wall.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+  puts("Hello, C");
+  return (0);
+}
---
+++ hello_world/2-hello_printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+  printf("Hello, C");
+  printf("\n");
+  return (0);
+}
