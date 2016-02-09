4a14e667cc7add189e772768941b120ea292ffe6
Carole Bou <35@p2-5.hbtn.io>
2016-02-08 23:55:19 -0800

    Complete task 1

---
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -0,0 +1,12 @@
+/* Includes library created in current directory */
+#include "my_functions.h"
+
+/* Function to print alphabet */
+void print_alphabet(void)
+{
+/* Declares variable c */
+  char c;
+/* Function to print characters from a to z */
+  for(c='a'; c<='z'; ++c)
+  print_char(c);
+}
--- i_m_not_in_the_loop/my_functions.h
+++ i_m_not_in_the_loop/my_functions.h
@@ -1,7 +1,13 @@
-/* This function prints characters */
+/* Below are prototypes used for functions
+ * This function prints characters
+ */
 int print_char(char c);
 /* This is the prototype of the function                                           
  * print_z                                                                          
  */
 void print_z(void);                                   
 void print_alphabet(void);
+void print_tebahpla(void);
+void print_base16(void);
+void positive_or_not(int n);
+void print_number(int n);
