9422e570d696d18ce2f60d21daaca116397fd702
Carole Bou <35@p2-1.hbtn.io>
2016-02-14 00:14:47 -0800

    Found length and power task 0

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,12 +1,29 @@
 /* include header containing prototypes
  */
-#include "my_functions"
+#include "my_functions.h"
+#include <stdio.h>
 
-void print_number(int n)
+void print_number(int n) 
 {
-  int n 
-  if 
-  for {c 0
-  print_number (print_char);
-  print_char ('c');
+  int nu = n;
+  int length;
+  int power = 1;
+  if(nu < 0) {
+    nu = nu * (-1), print_char('-');
+      }
+  for ( length = 0; nu>0; length++ ) {
+    nu = nu/10;
+  }
+    printf("%d\n", length);
+
+while(length > 1) {
+  power = power * 10;
+  length = length - 1;
+ }
+
+printf("power %d\n", power);
 }
+
+
+
+
