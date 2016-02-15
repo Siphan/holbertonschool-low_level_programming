15b991921527c258a6f2cd1de012c2684e9924ea
Carole Bou <35@p3-5.hbtn.io>
2016-02-14 22:43:16 -0800

    Completed mandatory task

---
+++ d/0-printf_number.c
@@ -0,0 +1,7 @@
+/* Contains prototype */
+#include "my_functions.h"
+
+/* Function that takes integer as an argument */ 
+void printf_number(int n){
+  printf("%d",n); /* Prints integers n as assigned  */
+}
---
+++ d/my_functions.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+/* Prototype */
+void printf_number(int n);
