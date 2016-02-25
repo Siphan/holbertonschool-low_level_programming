b07e242952bf5b54bd167fba25f7797bb80d0af7
Carole Bou <35@p3-6.hbtn.io>
2016-02-24 19:16:51 -0800

    Added comments to explain function first_digit

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,16 +1,25 @@
-int first_digit(int n)
+/* Defines the function called in main 
+   Takes int n as paramater, n is defined in main
+   Returns int o to main which will print o
+*/
+int first_digit(int n) 
 {
-  long int o;
-  o = n;
+  /* Needs long integer data type for the last number INT_MIN
+     to print as it's too long for Vagrant */
+  long int o; 
+  o = n; /* Reassigning o to n ie int n is now o which is a long int data type */
   
-  if (o < 0)
+  if (o < 0) /* If int o to be returned to main is negative, makes it positive  */
   {
     o = (o * -1);
   }
 
-  while (o > 9)
+  /* While loop prevents from creating new variable 
+     ie uses existing variable
+  */
+  while (o > 9) /* If number o has more than one digit, divide it by 10 */
   {
     o = (o / 10);
   }
-  return (o);
+  return (o); /* When o only has one digit, o is returned to main */
 }
