6dab162b4e28987c7f74b6b3f2f51f325cd6b01f
Siphan <carole.bou.ibms@gmail.com>
2016-03-25 05:41:47 +0000

    Created files

---
+++ let_s_go_deeper/0-factorial.c
@@ -0,0 +1,17 @@
+/* This function returns the factorial of a given number */
+int factorial(int n)
+{
+  n = r;
+  int i; /* Index */
+  if (n < 0) {
+    return (-1);
+  }
+  else if (n == 0) {
+    return (1);
+  }
+
+  for (i = 1; i <= n; i++) {
+    
+    return (n);
+  }  
+}
