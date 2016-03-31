01d772b6570cdeb2f9a01ccdb0d445c2bbea1bb9
Siphan <carole.bou.ibms@gmail.com>
2016-03-31 06:42:28 +0000

    Corrected task 3, using recursion

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -1,26 +1,28 @@
-int test_is_prime_number(int n, int i);
+/* This is a function that checks to see if a number n is a prime number */
 
-/* This function returns 1 if a number is a prime number, 0 otherwise */
-int is_prime_number(int n){
-  if( n < 2 ){
-    return 0;
-  }
-  else{
-    return test_is_prime_number(n, 2);
-  }
+int test_is_prime_number(int n,int i)
+{
+  int res; /* Result of n divided by i */
+  res = 0;
+
+  if (n >= 2 /* Starting with n = 2 as we know 1 is a prime number */)
+    {
+      if (i == 1) 
+	{
+	  res = 1;
+	}
+      else if (n % i != 0) /* If remainder of n divided by 1 is not 0, function calls itself */
+	{
+	  res = test_is_prime_number(n, i - 1);
+	}
+    }
+
+  return res;
 }
 
-/* This function tests if n is a multiple of i */
-int test_is_prime_number(int n, int i){
-  if ( i < n){
-    if ( n%i == 0 ){
-      return 0 ;
-    }
-    else{
-      return test_is_prime_number(n, i+1);
-    }
-  }
-  else {
-    return 1 ;
-  }
+/* This is a function that returns 1 if a number is a prime number, 0 otherwise. */
+
+int is_prime_number(int n)
+{
+  return test_is_prime_number(n, n/2);
 }
