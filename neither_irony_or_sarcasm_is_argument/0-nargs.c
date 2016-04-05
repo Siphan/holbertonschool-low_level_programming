e4a5730c79899d63ac606f8fa066c13f56437fbb
Siphan <carole.bou.ibms@gmail.com>
2016-04-05 04:20:14 +0000

    Corrected code using same var names as in instructions

--- neither_irony_or_sarcasm_is_argument/0-nargs.c
+++ neither_irony_or_sarcasm_is_argument/0-nargs.c
@@ -1,10 +1,10 @@
 /* This function prints the number of arguments you pass to it */
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  printf("%d\n", argc - 1);
-  if (argv[0] == 0) {
-    printf("Error... I think");
+int main(int ac, char **av) { /* ac is the argument counter, av a two dimensional array */
+  printf("%d\n", ac - 1); /* Prints the number of arguments passed after executing a.out */
+  if (av[0] == 0) { /* av[0] is the name of the program, or an empty string if the name is not available  */
+    printf("Error... I think\n");
     }
   return (0);
 }
