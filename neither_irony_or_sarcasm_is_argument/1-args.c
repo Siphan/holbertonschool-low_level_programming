38be41d839f18cd5a9ef4a718116813beb2f2879
Siphan <carole.bou.ibms@gmail.com>
2016-04-05 06:02:26 +0000

    Write verbose comments

--- neither_irony_or_sarcasm_is_argument/1-args.c
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -1,12 +1,12 @@
 /* This program prints all arguments it receives */
 #include <stdio.h>
 
-int main(int ac, char **av) {
+int main(int ac, char **av) { /* ac is the argument counter, av a two dimensional array */
 
-    int i;
+  int i; /* Declaring index */
 
-    for(i = 0; i < ac; i++) {
-      printf("%s\n", av[i]);
+  for(i = 0; i < ac; i++) { /* Loop to print all arguments passed to the program */
+    printf("%s\n", av[i]); /* Prints every argument (in this case, string) passed, followed by a new line, using array av */
     }
 
     return (0);
