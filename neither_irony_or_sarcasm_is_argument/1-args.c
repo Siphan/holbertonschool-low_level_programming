b505c2eae5cd23e7b9ca70798df046483227e02f
Siphan <carole.bou.ibms@gmail.com>
2016-04-05 05:49:28 +0000

    Completed mandatory tasks

---
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -0,0 +1,14 @@
+/* This program prints all arguments it receives */
+#include <stdio.h>
+
+int main(int ac, char **av) {
+
+    int i;
+
+    for(i = 0; i < ac; i++) {
+      printf("%s\n", av[i]);
+    }
+
+    return (0);
+}
+
