51f24cd1efad04dc8f4344d54c4dcc866e5e1cbe
Siphan <carole.bou.ibms@gmail.com>
2016-05-16 03:09:05 +0000

    Test execve

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -14,43 +14,40 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/* Prototypes in libshell.h - delete when done */
-char *read_line(const int fd);
-char **string_split(const char *str, char separator);
-
 int print_char(char c) {
   return (write(1, &c, 1));
 }
 
-int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, char **env) {
-char *line; /* Points to a malloced array */
+int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av, char **env) /* char **env inherits all the environment variables when it gets executed */
+{
+char *input; /* Points to a malloced array */
 char **args; /* Stores array of strings returned by string_split */
 pid_t pid;
 int status;
-char *exec_argv[] = {"/bin/ls", "-l", NULL}; /* Executes ls program with the option -l */
 
   do { /* Prints promt at least once */
     print_char('>');
     print_char(' ');
-    line = read_line(0); /* Reads stdin and stores output which is a malloced array */
-    args = string_split(line, ' '); /* Splits string from stdin into arguments to be interpreted */
+    input = read_line(0); /* Reads stdin and stores output which is a malloced array */
+    args = string_split(input, ' '); /* Splits string from stdin into arguments to be interpreted */
 /*
  * We need to fork to create a child process
  * that will execute non-built-in programs within our program
- * using the execve() function
+ * using the execve() function which executes a process
+ * in an environment which it assigns
  */
       if ((pid = fork()) == -1) { /* Handles error from forking */
         perror("fork");
         return (1);
       }
       if (pid == 0) { /* Handles child process */
-        execve(exec_argv[0], exec_argv, env);
+        execve(args[0], args, env); /* Executes the program that was typed as input with all its arguments */
       }
       else { /* Handles parent process */
         wait(&status);/* Waits for child to terminate before displaying prompt and read_line again */
-        free(line);
+        free(input);
         free(args);
       }
-  } while(0);
+  } while(status);
 return(0);
 }
