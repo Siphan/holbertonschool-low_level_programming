33864ecd579639789b0eb8bfde836544b1f48aa9
Siphan Bou <35@holbertonschool.com>
2016-05-17 00:41:28 -0700

    Delete len_command.c

--- simple_shell/len_command.c
+++
@@ -1,9 +0,0 @@
-#include "my_functions.h"
-int len_command(char **command) {
-  int length = 0;
-  while (*command != 0) {
-    length++;
-    command++;
-  }
-  return length; /* length of command */
-}
