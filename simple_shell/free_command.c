f5824670fabc7007a162a659b5325420c202fe19
Siphan Bou <35@holbertonschool.com>
2016-05-17 00:41:20 -0700

    Delete free_command.c

--- simple_shell/free_command.c
+++
@@ -1,9 +0,0 @@
-#include "my_functions.h"
-void free_command(char **command) { /* free */
-  int i = 0;
-  while (command[i] != 0) {
-    free(command[i]); /* free */
-    i++;
-  }
-  free(command);
-}
