b87a91b81d7f86de79f340ffcad808df4eca3b57
Siphan Bou <35@holbertonschool.com>
2016-07-11 01:04:58 -0700

    Update comments

--- hash_tables/hash_func.c
+++ hash_tables/hash_func.c
@@ -1,6 +1,5 @@
 /*
- * This is a simple implementation of the function hash if you want to test your functions.
- * Don't turn it in! This example will probably not be used during the correction.
+ * This is a simple implementation of the function hash to test functions.
  */
 unsigned int hash(const char *key, unsigned int size)
 {
