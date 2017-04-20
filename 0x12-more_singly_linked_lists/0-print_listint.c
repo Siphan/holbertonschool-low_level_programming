dee08f986b4212bf27a3ca73cdf7bddd22542dec
Siphan Bou <35@holbertonschool.com>
2017-04-20 19:23:33 +0200

    Add func to print list and make header file Betty compliant

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,39 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include "lists.h"
 
-size_t print_listint(const listint_t *h);
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * print_listint - print the data stored in each element of a list.
+ *
+ * @temp: pointer to node to traverse the list.
+ *
+ * Return: the number of nodes in a list.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+  Node* temp = h;
+  size_t n;
+
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+    printf("%d\n", temp->n);
+    m += 1;
+  }
+  return (m);
+}
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,2 +1,26 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 int _putchar(char c);
+
+/*
+ * print_listint - print the data stored in each element of a list.
+ * Return: the number of nodes in a list.
+ */
 size_t print_listint(const listint_t *h);
+
+#endif /* _LISTS_ */
