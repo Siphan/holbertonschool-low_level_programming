363907fc7d3ccb38b4e6ded23706393886373796
Siphan Bou <35@holbertonschool.com>
2017-04-20 19:27:29 +0200

    Correct func to print list

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -3,37 +3,26 @@
 #include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * print_listint - print the data stored in each element
+ * of a linked listint_t list and
+ * return the number of elements in that list.
  *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/**
- * print_listint - print the data stored in each element of a list.
- *
- * @temp: pointer to node to traverse the list.
+ * @h: head node in the singly linked list.
  *
  * Return: the number of nodes in a list.
  */
 
 size_t print_listint(const listint_t *h)
 {
-  Node* temp = h;
-  size_t n;
+  size_t m;
 
-  while (temp->next != NULL)
+  m = 0;
+
+  while (h != NULL)
   {
-    temp = temp->next;
-    printf("%d\n", temp->n);
-    m += 1;
+    printf("%d\n", h->n);
+    h = h->next;
+    m++;
   }
   return (m);
 }
