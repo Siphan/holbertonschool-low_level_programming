5651969d59f3fa61ce71d7ec0ab72ca682d81230
Siphan Bou <35@holbertonschool.com>
2017-04-21 01:52:56 +0200

    Fix indentation again

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,30 +11,30 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *new_node;
-  listint_t *temp;
+    listint_t *new_node;
+    listint_t *temp;
 
-  new_node = malloc(sizeof(listint_t));
+    new_node = malloc(sizeof(listint_t));
 
-  new_node->n = n;
-  new_node->next = NULL;
+    new_node->n = n;
+    new_node->next = NULL;
 
-  if (new_node == NULL)
-  {
-    return (NULL);
-  }
-  else if (*head == NULL)
-  {
-    *head = new_node;
-  }
-  else
-  {
-    temp = *head;
-    while (temp->next != NULL)
+    if (new_node == NULL)
     {
-      temp = temp->next;
+        return (NULL);
     }
-    temp->next = new_node;
-  }
-  return (temp);
+    else if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+    return (temp);
 }
