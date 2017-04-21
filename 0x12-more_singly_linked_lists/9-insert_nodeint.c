8dbf19b035fae5b34fc823d4d0d44023ad853a59
Siphan Bou <siphan.bou@gmail.com>
2017-04-21 04:27:33 +0200

    Add other missing empty line to pass style checks

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
+	
 	for (i = 0; current_node != NULL; i++)
 	{
 		if (idx == 0)
