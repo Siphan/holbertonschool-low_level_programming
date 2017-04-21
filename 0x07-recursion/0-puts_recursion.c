228afbaa4405599dd5c59e7f6d2cc39445246b3a
Siphan Bou <35@holbertonschool.com>
2017-04-21 17:54:52 +0200

    Print a string followed by a new line

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,2 +1,21 @@
 /* Function that prints a string, followed by a new line */
-void _puts_recursion(char *s);
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print a string to sdtout, followed by a new line
+ * @s: input string
+ *
+ * Return: nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+}
