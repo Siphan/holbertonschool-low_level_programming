7a9a368e2671ec2da7aa8c3ff592cad6b1a77db8
Siphan Bou <35@holbertonschool.com>
2017-04-21 18:18:03 +0200

    Print a string in reverse

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,2 +1,19 @@
 /* Function that prints a string in reverse */
 void _print_rev_recursion(char *s);
+
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse.
+ * @s: input string.
+ *
+ * Return: nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
