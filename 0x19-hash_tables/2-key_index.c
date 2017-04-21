ee6adce8ce99cfc50d6c2e6930da9590ad30aff4
Siphan Bou <35@holbertonschool.com>
2017-04-22 01:39:05 +0200

    Add func to look for index of key in hash table

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,4 +1,14 @@
 #include "hash_tables.h"
 
-/* key_index - Return the index of a key */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+#include "hash_tables.h"
+/**
+ * key_index - Return the index of a key in a hash table.
+ * @key: key to look for
+ * @size: size of the hash table
+ *
+ * Return: index of key in hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
