cda24992794d8d833d003040c2b878f0dc3c4378
Siphan Bou <35@holbertonschool.com>
2017-04-21 19:12:35 +0200

    Add recursive func that returns x raised to the power of y

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,2 +1,43 @@
-/* Function that returns the value of x raised to the power of y */
-int _pow_recursion(int x, int y);
+#include <limits.h>
+#include "holberton.h"
+
+/**
+ * _pow_recursion - return the value of x raised to the power of y.
+ * @x: input number.
+ * @y: exponent.
+ *
+ * Return: value of x raised to the power of y.
+ * If y is negative, return -1.
+ * If the result cannot be stored in an int, return -1.
+ */
+
+int _pow_recursion(int x, int y)
+{
+  int result = 0;
+
+  if (y < 0)
+  {
+    return (-1);
+  }
+
+  /* If exponent is 0, result is always 1 */
+  else if (y == 0)
+  {
+    return (1);
+  }
+
+  else
+  {
+    /* Recursive func returns x to the power of y. Continues if y >= 0 */
+    result = x * _pow_recursion(x, (y - 1));
+
+    /* If the result cannot be stored in an int, the function returns -1. */
+    if (result > INT_MAX)
+    {
+      return (-1);
+    }
+  }
+
+  /* result is returned to main() function to be printed to stdout */
+  return (result);
+}
