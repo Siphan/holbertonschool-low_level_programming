ac8d4e5c930bb3cd1cce81fc70111685af8f3eba
Siphan Bou <siphan.bou@gmail.com>
2017-04-21 04:21:34 +0200

    Fix index name

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,14 +26,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0; current_node != NULL; i++)
 	{
-		if (index == 0)
+		if (idx == 0)
 		{
 			new_node->n = n;
 			new_node->next = *head;
 			*head = new_node;
 			return (new_node);
 		}
-		else if (i == (index - 1))
+		else if (i == (idx - 1))
 		{
 			new_node->n = n;
 			new_node->next = current_node->next;
