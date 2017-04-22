387814144627829c80bc1897d2d511336e408fa3
Siphan Bou <35@holbertonschool.com>
2017-04-22 03:24:22 +0200

    Print hash table

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,4 +1,38 @@
+#include <stdio.h>
 #include "hash_tables.h"
 
-/* hash_table_print - Print a hash table. */
-void hash_table_print(const hash_table_t *ht);
+/**
+ * hash_table_print - print a hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, j;
+	hash_node_t *temp;
+
+  j = 0;
+	if (ht == NULL)
+	{
+		return;
+		printf("{}\n");
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		for (temp = ht->array[i]; temp; temp = temp->next)
+		{
+			if (j == 0)
+      {
+        printf("'%s': '%s'", temp->key, temp->value);
+      }
+      else
+      {
+        printf(", '%s': '%s'", temp->key, temp->value);
+      }
+      j++;
+		}
+	}
+	printf("}\n");
+}
