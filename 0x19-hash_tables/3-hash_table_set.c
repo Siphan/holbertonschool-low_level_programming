a896ffa311867e203f28088e3fad510abf83116e
Siphan Bou <35@holbertonschool.com>
2017-04-22 02:44:13 +0200

    Free nodes

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,32 +11,51 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	unsigned long int index, size;
+	hash_node_t *new_node, *temp;
+	unsigned long int index;
 
 	if ((ht == NULL) || (key == NULL) || (value == NULL))
 		return (0);
 
 	/* Find the index */
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	/* Add the key/value pair at the index */
-	if ((ht->array[index] != NULL) && (strcmp(ht->array[index]->key, key) == 0))
+	if (ht->array[index] != NULL)
 	{
-		ht->array[index]->value = strdup(value);
-		return (1);
+    temp = ht->array[index];
+    while ((temp != NULL) && (strcmp(temp->key, key) != 0))
+    {
+      temp = temp->next;
+    }
+    if ((temp != NULL) && (strcmp(temp->key, key) == 0))
+    {
+      free(temp->value);
+      temp->value = strdup(value);
+      return (1);
+    }
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-
   if (new_node == NULL)
 		return (0);
 
   new_node->key = strdup(key);
+  if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
 	new_node->value = strdup(value);
+  if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-  free(new_node);
 	return (1);
 }
