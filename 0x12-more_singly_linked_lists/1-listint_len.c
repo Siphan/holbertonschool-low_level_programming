454312ff2b0ceb5b5d9f7af3ded0ec15299a6b80
Siphan BOU <sbou@vente-privee.com>
2017-10-24 18:18:09 +0200

    Replace indent tabs by spaces and delete all commented code

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -9,14 +9,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-  size_t count;
+	size_t count;
 
-  count = 0;
+	count = 0;
 
-  while (h != NULL)
-  {
-    h = h->next;
-    count++;
-  }
-  return (count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
