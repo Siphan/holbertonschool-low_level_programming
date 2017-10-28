fbf14dd3ce2cb3def8d68db91279e6471c2ef2c7
Siphan BOU <sbou@vente-privee.com>
2017-10-28 12:45:45 +0200

    Remove commented code

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -7,21 +7,6 @@
  * Return: a pointer to the new head pointer
  */
 listint_t *reverse_listint(listint_t **head)
-// {
-// 	listint_t *list, *temp;
-
-// 	list = 0;
-// 	while (*head != NULL)
-// 	{
-// 		temp = (*head)->next;
-// 		(*head)->next = list;
-// 		list = (*head);
-// 		(*head) = temp;
-// 	}
-// 	(*head) = list;
-// 	return (*head);
-// }
-
 {
 	listint_t *current;
 	listint_t *next;
