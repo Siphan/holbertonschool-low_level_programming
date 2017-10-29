b991777cd08305177bc06abb5124567325973233
Siphan BOU <sbou@vente-privee.com>
2017-10-29 23:01:21 +0100

    Maze - create window and make it appear on screen

--- maze/src/main.c
+++ maze/src/main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
 /**
  * main - initialize an SDL2 instance
@@ -8,19 +9,23 @@
  */
 int main(void)
 {
+	/* The window we'll be rendering to */
 	SDL_Window *window;
 	SDL_Renderer *renderer;
+	/* The surface contained by the window */
+	SDL_Surface *screenSurface;
 
-	/* Initialize SDL */
+	/* Initialize SDL. Needed to call any SDL function. */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
+		/* SDL_GetError returns the latest error produced by any SDL function */
 		fprintf(stderr, "Unable to intialize SDL: %s\n", SDL_GetError());
 		return (1);
 	}
 
 	/* Create a new Window instance */
 	window = SDL_CreateWindow("A-MAZE-ING \\o/",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720, SDL_WINDOW_SHOWN);
 	if (window == NULL)
 	{
 		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
@@ -37,5 +42,24 @@ int main(void)
 		SDL_Quit();
 		return (1);
 	}
+
+	/* Get window surface */
+	screenSurface = SDL_GetWindowSurface(window);
+
+	/* Fill the surface white */
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+	/* Update the surface */
+	SDL_UpdateWindowSurface(window);
+
+	/* Wait two seconds */
+	SDL_Delay(5000);
+
+	/* Destroy window to free its memory */
+	SDL_DestroyWindow(window);
+
+	/* Quit SDL subsystems after everything is deallocated */
+	SDL_Quit();
+
 	return (0);
 }
